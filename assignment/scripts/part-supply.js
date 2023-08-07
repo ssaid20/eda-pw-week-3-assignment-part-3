97a5c319c6663f6c3611753296e8bd5d5ec63471
Said Said <ssaid20@icloud.com>
2023-08-06 22:15:13 -0500

    Showing supplyChanges

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -25,6 +25,7 @@ console.log(removedItem);
 
 // 5. A delivery of 25 more parts arrived. Add the value 25 to the end of the array
 console.log('5. Adding 25 to supplyChanges.');
+supplyChanges.push(25);
 
 // 6. Write a `for` loop that shows each value in the 'supplyChanges' array
 //    Use a console.log formatted as follows, where x is the value from the array
@@ -32,7 +33,16 @@ console.log('5. Adding 25 to supplyChanges.');
 //    - if the value is 0, log 'No Change.'
 //    - if the value is negative, format the log as 'Removed x parts.' 
 console.log('6. Showing supplyChanges...');
-
+for (let i = 0; i < supplyChanges.length; i++) {
+    const value = supplyChanges[i];
+    if (value > 0) {
+      console.log(`Added ${value} parts.`);
+    } else if (value === 0) {
+      console.log('No Change.');
+    } else {
+      console.log(`Removed ${Math.abs(value)} parts.`);
+    }
+  }  
 
 // STRETCH GOALS
 console.log('---  Stretch Goals  ---');
